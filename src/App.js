a0c8adc4d46b32b58fd1d2ad113c76be37209750
sumanaghosh <sumanaghosh822@gmail.com>
2024-06-10 22:32:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+
+
+function App() {
+  return (
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      
+    </div>
+  );
+}
+
+export default App;
